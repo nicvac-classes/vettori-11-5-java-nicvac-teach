5ed0ad2bd41bce38ddb36df694f4c3f0bcfbcff4
nicvac-teach <nicola.vacca@marconibari.edu.it>
2025-02-11 21:49:14 +0000

    soluzione

--- Esercizio.java
+++ Esercizio.java
@@ -2,25 +2,93 @@
 
 //Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
+import java.util.Random;
 
 // Classe principale, con metodo main
 class Esercizio {
+
+    //Creo l'oggetto per l'input da tastiera
+    private static Scanner input = new Scanner(System.in);
+    private static Random random = new Random();
+
     // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+    public static void main(String args[]) {
+        int n, i, j, coppie, cornici;
+        boolean coppiaTrovata, continuaRicerca;
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+        do {
+            System.out.println("Quanti listelli ci sono nella cassettiera?");
+            n = Integer.parseInt( input.nextLine() );
+        } while (n <= 0);
+        int[] v = new int[n];
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        i = 0;
+        while (i <= n - 1) {
+            v[i] = 4 + random.nextInt(7);
+            i = i + 1;
+        }
+        visualizzaVettore(v, n);
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        // Trovo le coppie nel vettore. Ogni volta che trovo una coppia la elimino dal vettore. Mi fermo quando il vettore ha meno di 2 elementi oppure non trovo più coppie.
+        coppie = 0;
+        continuaRicerca = true;
+        while (n >= 2 && continuaRicerca) {
+            // Non appena trovo una coppia per l'elemento i, interrompo la ricerca.
+            coppiaTrovata = false;
+            i = 0;
+            while (i <= n - 2 && coppiaTrovata == false) {
+                j = i + 1;
+                while (j <= n - 1 && coppiaTrovata == false) {
+                    if (v[i] == v[j]) {
+                        System.out.println("Coppia: " + v[i] + " - " + v[j]);
+                        // conto la coppia e la elimino dal vettore
+                        coppie = coppie + 1;
+                        coppiaTrovata = true;
+                        n = eliminaElemento(v, n, j);
+                    } else {
+                        j = j + 1;
+                    }
+                }
+                if (coppiaTrovata) {
+                    // Elimino la coppia, riparte la ricerca della nuova coppia
+                    n = eliminaElemento(v, n, i);
+                } else {
+                    i = i + 1;
+                }
+            }
+
+            // Se non ho trovato coppie, gli elementi restanti sono tutti distinti, quindi non proseguo oltre con la ricerca di coppie.
+            continuaRicerca = coppiaTrovata;
+        }
+        cornici = coppie / 2;
+        System.out.println("Si possono produrre " + cornici + " cornici.");
+        System.out.println("Listelli non accoppiati:");
+        visualizzaVettore(v, n);
     }
+
+    public static int eliminaElemento(int[] v, int n, int ie) {
+        int i, n2;
+
+        n2 = n - 1;
+        i = ie;
+        while (i <= n - 2) {
+            v[i] = v[i + 1];
+            i = i + 1;
+        }
+        
+        return n2;
+    }
+    
+    public static void visualizzaVettore(int[] v, int n) {
+        int i;
+
+        i = 0;
+        while (i < n) {
+            System.out.println("[" + i + "]: " + v[i]);
+            i = i + 1;
+        }
+    }
+
+
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
